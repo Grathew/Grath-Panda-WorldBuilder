4dfa9889283d9cae360e7c105ebff3163ce09f36
BattlePanda15 <rpmccormick15@gmail.com>
2018-12-31 13:55:55 -0600

    made habitable builder

--- starBuilder.py
+++ starBuilder.py
@@ -4,20 +4,20 @@ Generates stars for use in solar system creation
 
 import random
 import math
+def habitableStar():
+    mass = round(random.uniform(0.6,1.4),4)
+    diameter = round(mass**0.74,4)
+    surfaceTemp = round(mass**0.505,4)
+    lifeTime = round(mass**-2.5,4)
+    brightness = round(mass**3,4)
+    hz = round(math.sqrt(brightness),4)
+    ib = round(hz*0.95,4)
+    ob = round(hz*1.37,4)
 
-mass = round(random.uniform(0.6,1.4),4)
-diameter = round(mass**0.74,4)
-surfaceTemp = round(mass**0.505,4)
-lifeTime = round(mass**-2.5,4)
-brightness = round(mass**3,4)
-hz = round(math.sqrt(brightness),4)
-ib = round(hz*0.95,4)
-ob = round(hz*1.37,4)
-
-if surfaceTemp <= 3700:
-    color = "orangeRed"
-elif surfaceTemp <= 5200:
-    color = "yellowOrange"
-elif surfaceTemp <= 6000:
-    color = "yellowWhite"
-else color = "white"
+    if surfaceTemp <= 3700:
+        color = "orangeRed"
+    elif surfaceTemp <= 5200:
+        color = "yellowOrange"
+    elif surfaceTemp <= 6000:
+        color = "yellowWhite"
+    else color = "white"
