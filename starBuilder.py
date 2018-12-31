dd71ef85ad9079cf40762ac1ce6b164578e6d166
BattlePanda15 <rpmccormick15@gmail.com>
2018-12-31 13:07:12 -0600

    added priameters

--- starBuilder.py
+++ starBuilder.py
@@ -1,3 +1,12 @@
 """
 Generates stars for use in solar system creation
 """
+
+import random
+import math
+
+mass = round(random.uniform(0.6,1.4),4)
+diameter = round(mass**0.74,4)
+surfaceTemp = round(mass**0.505,4)
+lifeTime = round(mass**-2.5,4)
+brightness = round(mass**3,4)
